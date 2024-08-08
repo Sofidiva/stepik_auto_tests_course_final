7c7432c27448d59cd5b208e829224037ef169558
Sofidiva <sofiya.diveeva@gmail.com>
2024-08-08 16:53:54 +0500

    Переход между страницами (Первый способ: возвращать нужный Page Object). Исправление locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,15 @@
 from pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
-#link = 'http://selenium1py.pythonanywhere.com/'
-link = 'https://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+link = 'http://selenium1py.pythonanywhere.com/'
+#link = 'https://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                     # открываем страницу
-    page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
